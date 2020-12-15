322d13c8d2419c2441813de4ef18f07bc56d2305
G L Mbella Essomba <fred_leschener@hotmail.com>
2020-12-15 08:59:14 +0000

    update config file

--- code/rds_config.py
+++ code/rds_config.py
@@ -1,4 +1,4 @@
-rds_host  = "rds-auroramysql-demodb.cluster-c9di9qmu8xqw.us-east-1.rds.amazonaws.com"
-#rds_host = "rds-proxy-demo.proxy-c9di9qmu8xqw.us-east-1.rds.amazonaws.com"
-db_username = "admin"
-db_password = "admin123"
+#rds_host  = "rds-auroramysql-demodb.cluster-c9di9qmu8xqw.us-east-1.rds.amazonaws.com"
+rds_host = "rds-proxy-demo.proxy-c9di9qmu8xqw.us-east-1.rds.amazonaws.com"
+db_username = "<replace-with-your-db-user>"
+db_password = "<replace-with-your-db-password>"
